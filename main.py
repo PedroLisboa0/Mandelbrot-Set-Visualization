60c98a8c9a83e60a8bbf704bd9fa7227121af0d0
Pedro Lisboa <lisboa.pedrohenrique1@gmail.com>
2025-04-24 09:34:40 -0300

    Fixed aspect ratio.

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ import numpy as np
 from equation import mandelbrot, map_value
 
 
-
-width = 2000
-height = 2000
+width = 3840
+height = 2160
 
 white = 255
 black = 0
@@ -13,14 +12,22 @@ black = 0
 canvas = Image.new(mode="L", size=(width, height))
 pixels = canvas.load()
 
+
+real_start, real_end = -2.0, 1.0
+
+# Adjust vertical (imaginary) range based on aspect ratio
+aspect_ratio = height / width
+im_center = 0.0
+im_range = (real_end - real_start) * aspect_ratio / 2
+im_start = im_center - im_range
+im_end = im_center + im_range 
+
 for y in range(height):
     for x in range(width):
-        a = map_value(x, 0, width, -2, 1)
-        b = map_value(y, 0, height, -1.5, 1.5)
-        explodes = mandelbrot(a, b, max_iterations=100)
+        a = map_value(x, 0, width, real_start, real_end)
+        b = map_value(y, 0, height, im_start, im_end)
+        explodes = mandelbrot(a, b, max_iterations=250)
         if not explodes:
-            pixels[x+25, y] = white
+            pixels[x, y] = white
 
-
-   
-canvas.save("mandelbrot_100i.jpg")
+canvas.save("mandelbrot_250i_correct_aspect.jpg")
